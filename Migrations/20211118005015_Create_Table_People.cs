83d0d27137c775af66ea9968192d54eee8627156
lehieu099 <duyhieu099a2@gmail.com>
2021-11-18 08:15:19 +0700

    Inheritance

---
+++ Migrations/20211118005015_Create_Table_People.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Create_Table_People : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PersonName = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    StudentCode = table.Column<int>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    University = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Student : Person
+    {
+        public int StudentCode { get; set; }
+        [DisplayName("Địa Chỉ")]
+        public string Address { get; set; }
+        [DisplayName("Trường")]
+        public string University{get; set;}
+
+    }
+}
